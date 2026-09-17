8b636345941c2f38e3fb1c475f3fa9313aca1a3b
Michelle Tan <michelletanjm@gmail.com>
2026-09-17 23:05:03 +0800

    feat: add new icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { NavLink, Outlet } from 'react-router-dom'
+import logo from './assets/logo.svg'
 import SignOutButton from './components/SignOutButton.jsx'
 import UserSwitcher from './components/UserSwitcher.jsx'
 import './App.css'
@@ -7,7 +8,10 @@ function App() {
   return (
     <>
       <header className="app-header">
-        <h1>foodie</h1>
+        <h1>
+          <img src={logo} alt="" width="28" height="28" />
+          foodie
+        </h1>
         <nav>
           <NavLink to="/" end>
             Recipes
